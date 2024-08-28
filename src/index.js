58ab306f8f9f2d31917fca4866da468c91d5a8ae
Sakthivishnum <vishnugavi2003@gmail.com>
2024-08-28 07:04:49 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Chessboard() {
+  const boardData = [
+    ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
+    ['♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', ''],
+    ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
+    ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'],
+  ];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const renderSquare = (row, col) => {
+    const isLightSquare = (row + col) % 2 === 0;
+    const piece = boardData[row][col];
+    return (
+      <td key={`${row}-${col}`} className={isLightSquare ? 'light' : 'dark'}>
+        {piece}
+      </td>
+    );
+  };
+
+  return (
+    <div className="chessboard">
+      <table>
+        <tbody>
+          {boardData.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              <th>{8 - rowIndex}</th>
+              {row.map((piece, colIndex) => renderSquare(rowIndex, colIndex))}
+              <th>{8 - rowIndex}</th>
+            </tr>
+          ))}
+          <tr>
+            <th></th>
+            <th>a</th>
+            <th>b</th>
+            <th>c</th>
+            <th>d</th>
+            <th>e</th>
+            <th>f</th>
+            <th>g</th>
+            <th>h</th>
+            <th></th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+ReactDOM.render(<Chessboard />, document.getElementById('root'));
